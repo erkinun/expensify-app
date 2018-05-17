ae50b3df7a77558e4e7db38e68041d672c3f5293
lost_mohican <erkinun@gmail.com>
2018-05-17 23:13:15 +0100

    using find, change, state in tests

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import ExpenseForm from '../../components/ExpenseForm'
 import expenses from '../fixtures/expenses'
+import { wrap } from 'module';
 
 test('should render ExpenseForm correctly', () => {
     const wrapper = shallow(<ExpenseForm />)
@@ -11,4 +12,49 @@ test('should render ExpenseForm correctly', () => {
 test('should render Expense Form with some data', () => {
     const wrapper = shallow(<ExpenseForm expense={expenses[0]}/>)
     expect(wrapper).toMatchSnapshot()
+})
+
+test('should render error for invalid form submission', () => {
+    const wrapper = shallow(<ExpenseForm />)
+    wrapper.find('form').simulate('submit', {
+        preventDefault: () => {}
+    })
+    expect(wrapper.state('error').length).toBeGreaterThan(0)
+    expect(wrapper).toMatchSnapshot()
+})
+
+test('should set description on input change', () => {
+    const value = "New Description"
+    const wrapper = shallow(<ExpenseForm />)
+    wrapper.find('input').at(0).simulate('change', {
+        target: { value }
+    })
+    expect(wrapper.state('description')).toBe(value)
+})
+
+test('should set note on input change', () => {
+    const value = "Some note"
+    const wrapper = shallow(<ExpenseForm />)
+    wrapper.find('textarea').at(0).simulate('change', {
+        target: { value }
+    })
+    expect(wrapper.state('note')).toBe(value)
+})
+
+test('should set valid amount', () => {
+    const value = "23.45"
+    const wrapper = shallow(<ExpenseForm />)
+    wrapper.find('input').at(1).simulate('change', {
+        target: { value }
+    })
+    expect(wrapper.state('amount')).toBe(value)
+})
+
+test('should not set invalid amount', () => {
+    const value = "23.45213"
+    const wrapper = shallow(<ExpenseForm />)
+    wrapper.find('input').at(1).simulate('change', {
+        target: { value }
+    })
+    expect(wrapper.state('amount')).toBe('')
 })
