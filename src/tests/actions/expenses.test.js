13977d5bf5400bd51a7bacd8d584d90a4bde487d
lost_mohican <erkinun@gmail.com>
2018-06-17 12:59:59 +0100

    adding format for money and date

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,7 +1,7 @@
 import { addExpense, editExpense, removeExpense } from '../../actions/expenses'
 
 test('should setup remove expense action object', () => {
-    const action = removeExpense({id: '123abc'})
+    const action = removeExpense('123abc')
     expect(action).toEqual({
         type: 'REMOVE_EXPENSE',
         id: '123abc'
