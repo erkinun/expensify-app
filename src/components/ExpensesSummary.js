50c4e9d54bb2385a42226ddfe1ae95d6eacd85b5
lost_mohican <erkinun@gmail.com>
2018-06-20 22:36:52 +0100

    refactor to group values

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -4,11 +4,16 @@ import selectExpenses from '../selectors/expenses'
 import expenseTotals from '../selectors/expenses-total'
 import numeral from 'numeral'
 
-export const ExpensesSummary = (props) => (
-    <div>
-        Viewing {props.expenses.length} {props.expenses.length > 1 ? 'expenses': 'expense'} totalling {numeral(expenseTotals(props.expenses) / 100).format('$0,0.00')} 
-    </div>
-)
+export const ExpensesSummary = (props) => {
+    const expenseCount = props.expenses.length
+    const expenseWord = expenseCount > 1 ? 'expenses' : 'expense'
+    const total = numeral(expenseTotals(props.expenses) / 100).format('$0,0.00')
+    return (
+        <div>
+            <h1>Viewing {expenseCount} {expenseWord} totalling {total} </h1>
+        </div>
+    )
+}
 
 const mapStateToProps = (state) => {
     return {
