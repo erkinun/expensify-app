b92882879192bdbdccdd4ee10c728decd54091e1
erkinun <erkin.unlu@quantemplate.com>
2018-07-24 11:00:18 +0100

    added mock store and async tests

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,5 +1,9 @@
-import { addExpense, editExpense, removeExpense } from '../../actions/expenses'
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import { addExpense, editExpense, removeExpense, startAddExpense } from '../../actions/expenses'
 import expenses from '../fixtures/expenses'
+
+const createMockStore = configureMockStore([thunk])
 
 test('should setup remove expense action object', () => {
     const action = removeExpense('123abc')
@@ -23,5 +27,20 @@ test('should setup add expense object with provided values', () => {
     expect(action).toEqual({
         type: 'ADD_EXPENSE',
         expense: expenses[2]
+    })
+})
+
+test('should add expense to database and store', (done) => {
+    const store = createMockStore({})
+    const expenseData = {
+        description: 'Mouse',
+        amount: 3000,
+        note: 'This one is better',
+        createAt: 1000
+    }
+
+    store.dispatch(startAddExpense(expenseData)).then(() => {
+        expect(1).toBe(1)
+        done()
     })
 })
