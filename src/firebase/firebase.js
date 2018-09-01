270b3aead25e85bf816fea6b11a0a609d4d6ba73
lost_mohican <erkinun@gmail.com>
2018-09-01 18:02:37 +0100

    clean firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,4 @@
 import * as firebase from 'firebase'
-//import expenses from '../tests/fixtures/expenses'
 
 const config = {
     apiKey: process.env.FIREBASE_API_KEY,
@@ -14,68 +13,3 @@ firebase.initializeApp(config);
 const database = firebase.database()
 
 export { firebase, database as default }
-
-/* /* db.ref('expenses').push(expenses[0])
-db.ref('expenses').push(expenses[1])
-db.ref('expenses').push(expenses[2]) */
-
-/* var expenses = []*/
-
-/* db.ref('expenses').on('value', (snapshot) => {
-    snapshot.forEach((childSnapshot) => {
-        expenses.push({
-            ...childSnapshot.val(),
-            id: childSnapshot.key
-        })
-    })
-
-    console.log(expenses)
-}) */
-
-/* db.ref('expenses').on('child_changed', (childSnapshot) => {
-    console.log(childSnapshot.key, childSnapshot.val())
-})
- */
-/* db.ref().set({
-    name: 'Erkin Unlu',
-    age: 32,
-    married: false,
-    location: {
-        city: 'London',
-        country: 'UK'
-    },
-    job: 'Software Developer',
-    company: 'QT'
-}).then(() => {
-    console.log('Data is saved to firebase')
-}).catch((e) => {
-    console.log('This failed', e)
-})
-
-db.ref('/attributes').set({
-    'height': 186,
-    'weight': 83
-}).then(() => {
-    console.log('Height and weight are set')
-}).catch((e) => {
-    console.log('There is something wrong', e)
-})
-
-
-db.ref().on('value', (snapshot) => {
-        const data = snapshot.val()
-        console.log(`${data.name} is a ${data.job} at ${data.company}`)
-})
-
-db.ref().update({
-    name: 'Andrew Marr'
-})
- */
-/* db.ref().once('value')
-    .then((snapshot) => {
-        const data = snapshot.val()
-        console.log(`${data.name} is a ${data.job} at ${data.company}`)
-    })
-    .catch((e) => {
-        console.log('Error fetching data', e)
-    }) */
